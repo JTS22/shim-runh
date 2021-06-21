0b3277349c7a31336fb2cbee2510e50d93e2f2bf
Jonas Schroeder <jonas.schroeder1@rwth-aachen.de>
2021-06-21 13:23:07 +0200

    call runh instead of runc

--- runtime/container.go
+++ runtime/container.go
@@ -1,19 +1,16 @@
-// +build linux
-
 /*
    Copyright The containerd Authors.
-
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
-
        http://www.apache.org/licenses/LICENSE-2.0
-
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
+
+   File modified by Jonas Schroeder for shim-runh
 */
 package runtime
 
@@ -57,7 +54,7 @@ func NewContainer(ctx context.Context, platform stdio.Platform, r *task.CreateTa
 	}
 
 	// Override default binary name to start runh instead of runc
-	//opts.BinaryName = "runh"
+	opts.BinaryName = "runh"
 
 	var mounts []process.Mount
 	for _, m := range r.Rootfs {
